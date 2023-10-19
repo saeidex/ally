3245ac67bbd1b1e5cbba6302f3956aea06ea0c91
saeidex <saeidex.dev@gmail.com>
2023-10-20 01:54:09 +0600

    style: hero-section

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ['"Bricolage Grotesque"', "sans-serif"],
+        nabla: ["Nabla"],
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [
+    // require("@tailwindcss/typography"),
+    // require("@tailwindcss/forms"),
+    // require("@tailwindcss/aspect-ratio"),
+    // require("@tailwindcss/container-queries"),
+  ],
+};
