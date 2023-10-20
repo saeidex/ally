c1c12d5f75b87e034a0141b0e3fd04cf47dc678e
saeidex <saeidex.dev@gmail.com>
2023-10-20 19:40:24 +0600

    modified:   src/index.html 	modified:   src/tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -3,10 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      fontFamily: {
-        display: ['"Bricolage Grotesque"', "sans-serif"],
-        nabla: ["Nabla"],
-      },
+      // fontFamily: {
+      //   bricolage: ['"Bricolage Grotesque"', "sans-serif"],
+      // },
     },
   },
   plugins: [
